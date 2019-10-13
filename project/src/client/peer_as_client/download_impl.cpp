0aa6376979cc5ce0b75ca18a75a2001ee4475eb7
ratnakar.mallayagari <ratnakar.mallayagari@gmail.com>
2019-10-13 06:02:45 +0530

    gathering available pieces for each seeder for piece selection

--- project/src/client/peer_as_client/download_impl.cpp
+++ project/src/client/peer_as_client/download_impl.cpp
@@ -112,30 +112,37 @@ char* download_impl(char* group_id, char* file_name, char* dest_path, char* user
 }
 
 unordered_map<string, string> piece_selection(Value file_info_root, piece_info_struct &piece_info_ctx){
-	cout <<"ENTERING PIECE SELECTIO ALGO" << endl;
-	unordered_map<string, Value> seeders_pieces_map = *piece_info_ctx.pieces_roots_val;
+	cout <<"PIECE SELECTIO ALGO" << endl;
+	unordered_map<string, Value> seeders_pieces_info_map = *piece_info_ctx.pieces_roots_val;
+	unordered_map<string, pair<vector<int>,Value>> seeders_pieces_map;
 	if(file_info_root.isMember("seeders")){
-    	Value seeders = file_info_root["seeders"];
-    	vector<string> members = seeders.getMemberNames();
-    	for(string seeder : members){
-    		Value piece_info = seeders_pieces_map[seeder];
-    		cout << "address: " << piece_info["address"] << endl;
+    	Value seeders_json = file_info_root["seeders"];
+    	vector<string> seeders = seeders_json.getMemberNames();
+    	for(string seeder : seeders){
+    		Value piece_info = seeders_pieces_info_map[seeder]["pieces"];
+    		vector<string> pieces_idx = piece_info.getMemberNames();
+    		vector<int> pieces_idx_int;
+    		for(string piece_str : pieces_idx){
+    			pieces_idx_int.push_back(atoi(piece_str.c_str()));
+    		}
+    		pair<vector<int>,Value> p = make_pair(pieces_idx_int, piece_info);
+    		seeders_pieces_map[seeder]=p;
+    		//cout << "address: " << piece_info["address"] << endl;
     	}
-    	cout << "size: " << (*piece_info_ctx.piece_info_stats).size() << endl;
-		cout << members[0] << ": " << *((*piece_info_ctx.piece_info_stats)[members[0]])<<endl;
-		cout << "file size" << ": " << (((*piece_info_ctx.pieces_roots_val)[members[0]]))["size"]<<endl;
-		
-		int file_size = (((*piece_info_ctx.pieces_roots_val)[members[0]]))["size"].asInt();
-	    int num_pieces = (((*piece_info_ctx.pieces_roots_val)[members[0]]))["total_pieces"].asInt();
+    	
+    	for ( auto it = seeders_pieces_map.begin(); it != seeders_pieces_map.end(); ++it )
+    	{
+    		string seeder = it->first;
+    		pair<vector<int>,Value> p = it->second;
+    		vector<int> available_pieces = p.first;
+    		cout << "*************printing available pieces in seeder: " << seeder << "****************" << endl;
+    		for (vector<int>::iterator it = available_pieces.begin() ; it != available_pieces.end(); ++it)
+    				cout << *it << " ";
+    		cout << endl;
 
-	    for(int i=0; i<num_pieces; i++){
-	    	int piece_size = (((*piece_info_ctx.pieces_roots_val)[members[0]]))["pieces"][to_string(i)]["piece_size"].asInt();
-		    string piece_sha1 = (((*piece_info_ctx.pieces_roots_val)[members[0]]))["pieces"][to_string(i)]["piece_sha1"].asString();
-	    	cout << "piece_size: " << piece_size << endl;
-	    	cout << "piece_sha1: " << piece_sha1 << endl;
-	    }
+    	}
+    	
 	}
-    cout <<"EXITING PIECE SELECTION ALGO" << endl;
     unordered_map<string, string> m;
 	return m;
 }
